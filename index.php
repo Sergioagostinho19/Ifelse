e9c8d7327e453e21be24a96664376665d6eb93d9
Sergioagostinho19 <150629145+Sergioagostinho19@users.noreply.github.com>
2026-04-03 13:25:15 -0300

    Add files via upload
    Exercicio 2 Professor Renan

--- index.php
+++ index.php
@@ -1,48 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-echo "<p> IMC </P>";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculadora IRPF 2026</title>
+</head>
 
-$peso = 70;
-$altura = 1.70;
+<body>
 
-$imc = $peso / $altura ** 2; 
+    <div class="caixa">
+
+        <h1>Calculadora IRFP 2026</h1>
+
+        <form method="POST">
+            <label for="salario"></label>
+
+            <input type="number" id="salario" placeholder="Insira seu salário..." step="0.001" name="salario" autofocus required>
+        </form>
+        <div class="salario">
+
+            <?php
 
 
-$imc = 39;
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-echo "O IMC é: $imc";
+                if (isset($_POST["salario"])) {
+
+                    $salario = $_POST["salario"];
+                }
+            }
+
+            if (isset($salario)) {
 
 
-if ($imc < 16) {
-echo " Magreza grau III";
-}
-else if ($imc >= 16 && $imc <= 16.9 ) {
-echo " Magreza grau II";
-}
+                if ($salario <= 2259.20) {
+                    echo "<br> A alíquota é 0%. A parcela deduzida do IR é R$0,00";
+                } else if ($salario > 2259.20 && $salario <= 2826.65) {
+                    echo "<br> A alíquota é 7,5%. A parcela deduzida do IR é R$169,44";
+                } else if ($salario > 2826.65 && $salario <= 3751.05) {
+                    echo "<br> A alíquota é 15%. A parcela deduzida do IR é R$381,44";
+                } else if ($salario > 3751.05 && $salario <= 4664.68) {
+                    echo "<br> A alíquota é 22,5%. A parcela deduzida do IR é R$662,77";
+                } else if ($salario > 4664.68) {
+                    echo "<br> A alíquota é 27,5%. A parcela deduzida do IR é R$896";
+                }
+            }
 
-else if ($imc >= 17 && $imc <= 18.4 ) {
-echo " Magreza grau I";
-}
+            ?>
 
-else if ($imc >= 18.5 && $imc <= 24.9 ) {
-echo " Eutrofia";
-}
 
-else if ($imc >= 25 && $imc <= 29.9 ) {
-echo " Pré-obesidade";
-}
+        </div>
+    </div>
 
-else if ($imc >= 30 && $imc <= 34.9 ) {
-echo " Obesidade moderada (grau I)";
-}
 
-else if ($imc >= 35 && $imc <= 39.9 ) {
-echo " Obesidade severa (grau II)";
-}
+</body>
 
-else{
-echo " Obesidade muito severa (grau III)";
 
-}
-
-?>
+</html>
